9445c5a7b7cb472c2450bc05ea1a621b66f50876
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2025-06-04 21:16:46 -0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,7 +17,7 @@ import { useEffect } from "react";
 export default function RootLayout() {
   return(
         <Stack>
-            <Stack.Screen name="auth" options={{headerShown: false}} />
+            <Stack.Screen name="(tabs)" options={{headerShown: false}} />
         </Stack>
   );
 }
